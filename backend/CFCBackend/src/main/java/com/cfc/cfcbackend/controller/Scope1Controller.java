bf2cae7a90bc637ec7d6271ec12ee5bcb86276e2
pengyu <pengyuchen01@gmail.com>
2025-02-10 23:38:04 -0500

    try to build route in backend

--- backend/CFCBackend/src/main/java/com/cfc/cfcbackend/controller/Scope1Controller.java
+++ backend/CFCBackend/src/main/java/com/cfc/cfcbackend/controller/Scope1Controller.java
@@ -2,19 +2,25 @@ package com.cfc.cfcbackend.controller;
 
 import com.cfc.cfcbackend.service.StationaryCombustionService;
 import jakarta.annotation.Resource;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class Scope1Controller {
     @Resource
     StationaryCombustionService stationaryCombustionService;
-
+    @ResponseBody
+    @GetMapping("/")
+    public String root() {
+        return "Welcome to the Carbon Footprint Calculator API";
+    }
+    
     @ResponseBody
     @GetMapping("scope1")
     public double scope1() {
         return stationaryCombustionService.naturalGasCO2(5000);
     }
+
+ 
 }
