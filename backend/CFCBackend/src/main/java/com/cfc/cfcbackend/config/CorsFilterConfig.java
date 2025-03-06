9eab0360a137fbe8cf23de9227100c4a4b8999df
pengyu <pengyuchen01@gmail.com>
2025-03-06 17:04:41 -0500

    fix 127.0.0.1:5173 not allow to access backend

--- backend/CFCBackend/src/main/java/com/cfc/cfcbackend/config/CorsFilterConfig.java
+++ backend/CFCBackend/src/main/java/com/cfc/cfcbackend/config/CorsFilterConfig.java
@@ -12,7 +12,9 @@ public class CorsFilterConfig implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
+        
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: "localhost" // 强制 Vite 绑定到 localhost，而不是 127.0.0.1
+  }
 })
