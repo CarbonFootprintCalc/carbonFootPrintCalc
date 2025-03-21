8314bc5520e8b2e16fa54ad68c0cf0056c9aef35
pengyu <pengyuchen01@gmail.com>
2025-03-21 16:04:42 -0400

    update the gas type

--- frontend/src/components/RefrigerationACFuelSelection.tsx
+++ frontend/src/components/RefrigerationACFuelSelection.tsx
@@ -17,7 +17,9 @@ interface RefrigerationSource {
   emissions?: number;
 }
 
-const gasOptions = ["HFC-23", "HFC-32", "CO2", "SF6"];
+const gasOptions = ["Carbon dioxide", "Methane", "Nitrous oxide", "HFC-23", "HFC-32", "HFC-41", "HFC-125", "HFC-134", "HFC-134a", "HFC-143", "HFC-143a", "HFC-152", "HFC-152a", "HFC-161", "HFC-227ea", "HFC-236cb", "HFC-236ea", "HFC-236fa", "HFC-245ca", "HFC-245fa", "HFC-365mfc", "HFC-43-10mee", "Sulfur hexafluoride", "Nitrogen trifluoride", "PFC-14", "PFC-116", "PFC-218", "PFC-318", "PFC-31-10", "PFC-41-12", "PFC-51-14", "PFC-91-18",
+"R-401A", "R-401B", "R-401C", "R-402A", "R-402B", "R-403B", "R-404A", "R-406A", "R-407A", "R-407B", "R-407C", "R-407D", "R-408A", "R-409A", "R-410A", "R-410B", "R-411A", "R-411B", "R-414A", "R-414B", "R-417A", "R-422A", "R-422D", "R-424A", "R-426A", "R-428A", "R-434A", "R-507A", "R-508A", "R-508B"
+ ];
 
 const RefrigerationACFuelSelection: React.FC<RefrigerationACFuelSelectionProps> = ({
   title,
