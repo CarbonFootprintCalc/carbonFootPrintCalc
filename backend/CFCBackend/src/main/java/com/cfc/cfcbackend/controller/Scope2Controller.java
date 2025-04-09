7f9d4818bd4e6d228096d55d20ef236d1a2b1a49
Ricky Wu <rickywu1231@gmail.com>
2025-04-09 19:32:06 -0400

    :sparkles: Implemented purchased steam calculations

--- backend/CFCBackend/src/main/java/com/cfc/cfcbackend/controller/Scope2Controller.java
+++ backend/CFCBackend/src/main/java/com/cfc/cfcbackend/controller/Scope2Controller.java
@@ -19,6 +19,8 @@ public class Scope2Controller {
 
     @Resource
     PurchasedElectricityService purchasedElectricityService;
+    @Resource
+    PurchasedSteamService purchasedSteamService;
 
     // Method to calculate emissions for purchased electricity
     @ResponseBody
@@ -39,6 +41,42 @@ public class Scope2Controller {
             emissions.put("co2", purchasedElectricityService.purchElecCO2(electricityPurchased, co2));
             emissions.put("ch4", purchasedElectricityService.purchElecCH4(electricityPurchased, ch4));
             emissions.put("n2o", purchasedElectricityService.purchElecN2O(electricityPurchased, n2o));
+        }
+        return emissions;
+    }
+
+    // Method to calculate emissions for purchased steam
+    @ResponseBody
+    @GetMapping("/steam")
+    public Map<String, Double> purchasedSteam(@RequestParam double steamPurchased, @RequestParam(required = false) String fuelType,
+                                              @RequestParam(required = false) double boilerEfficiency, @RequestParam double Lco2, 
+                                              @RequestParam double Lch4, @RequestParam double Ln2o, @RequestParam(required = false) double Mco2,
+                                              @RequestParam(required = false) double Mch4, @RequestParam(required = false) double Mn2o) {
+                                    
+        Map<String, Double> emissions = new HashMap<>();
+
+        // Set boiler efficiency to 80% if not provided
+        if(boilerEfficiency == 0) {
+            boilerEfficiency = 80;
+        }
+
+        // If the user selects a preset fuel type, calculate emissions using that fuel type
+        if(fuelType.isEmpty()) {
+            emissions = purchasedSteamService.purchSteamFuelType(steamPurchased, fuelType, boilerEfficiency);
+        }
+
+        // Otherwise, the user can directly input emission factors
+        else {
+
+            // Calculate location-based emissions
+            emissions.put("Lco2", purchasedSteamService.purchSteamCO2(steamPurchased, Lco2, boilerEfficiency));
+            emissions.put("Lch4", purchasedSteamService.purchSteamCH4(steamPurchased, Lch4, boilerEfficiency));
+            emissions.put("Ln2o", purchasedSteamService.purchSteamN2O(steamPurchased, Ln2o, boilerEfficiency));
+
+            // Calculate market-based emissions
+            emissions.put("Mco2", purchasedSteamService.purchSteamCO2(steamPurchased, Mco2, boilerEfficiency));
+            emissions.put("Mch4", purchasedSteamService.purchSteamCH4(steamPurchased, Mch4, boilerEfficiency));
+            emissions.put("Mn2o", purchasedSteamService.purchSteamN2O(steamPurchased, Mn2o, boilerEfficiency));
         }
         return emissions;
     }
