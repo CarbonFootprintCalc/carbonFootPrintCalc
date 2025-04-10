ecd691a10fcab1614a344adcdf2ebdd0b69efc39
WeiLuo-1 <luo.1310@osu.edu>
2025-04-09 20:07:37 -0400

    connect to 3 new tables in database, calculation complete

--- backend/CFCBackend/src/main/java/com/cfc/cfcbackend/service/BusinessTravelService.java
+++ backend/CFCBackend/src/main/java/com/cfc/cfcbackend/service/BusinessTravelService.java
@@ -1,12 +1,24 @@
 package com.cfc.cfcbackend.service;
 
+import com.cfc.cfcbackend.db.dao.BusinessTravelDao;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class BusinessTravelService {
 
-    // Calculation for business travel emission
-    public double calcEmission(String vehicleType, double mileage) {
-        return 0;
+    @Resource
+    private BusinessTravelDao businessTravelDao;
+
+    // Calculation for business travel emission for co2, ch4, n2o
+    public Map<String, Double> calcEmission(String vehicleType, double mileage) {
+        Map<String, Double> emissions = new HashMap<>();
+        emissions.put("co2", mileage * businessTravelDao.selectByVehicleType(vehicleType).getCo2FactorKgco2PerUnit());
+        emissions.put("ch4", mileage * businessTravelDao.selectByVehicleType(vehicleType).getCh4FactorGch4PerUnit());
+        emissions.put("n2o", mileage * businessTravelDao.selectByVehicleType(vehicleType).getN2oFactorGn2oPerUnit());
+        return emissions;
     }
 }
