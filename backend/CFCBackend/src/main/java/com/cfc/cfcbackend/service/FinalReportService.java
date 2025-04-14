3a6a8f31f3310047af335f9f48aad8e1200c1cd1
Ricky Wu <rickywu1231@gmail.com>
2025-04-14 19:43:34 -0400

    :construction: Setup final report service and controller

---
+++ backend/CFCBackend/src/main/java/com/cfc/cfcbackend/service/FinalReportService.java
@@ -0,0 +1,19 @@
+package com.cfc.cfcbackend.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class FinalReportService {
+    
+    // Method to sum together all values
+    public double sumValues() {
+        return 0;
+    }
+
+    // Method to convert an emission into CO2-e
+    public double convertToCO2e(double emission) {
+        return 0;
+    }
+}
