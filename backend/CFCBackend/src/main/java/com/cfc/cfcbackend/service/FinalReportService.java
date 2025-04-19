e4faf17ab777c7a0dd073736b72cf5688fdef27d
Ricky Wu <rickywu1231@gmail.com>
2025-04-18 21:58:27 -0400

    :sparkles: Converting final report results into metric tons

--- backend/CFCBackend/src/main/java/com/cfc/cfcbackend/service/FinalReportService.java
+++ backend/CFCBackend/src/main/java/com/cfc/cfcbackend/service/FinalReportService.java
@@ -10,33 +10,35 @@ import javax.annotation.Resource;
 @Service
 public class FinalReportService {
     
+    // Gas GWPS
     private final double CH4_GWP = 28;
     private final double N2O_GWP = 265;
 
-    private final double G_TO_KG = 1000;
+    // Convert g-to-kg and kg-to-metric ton
+    private final double INC_METRIC_MAGNITUDE = 1000;
 
     // We don't need to convert CO2 to CO2e as the conversion is 1-to-1
 
     // Method to convert CH4 emissions to CO2e in kg
     public double convertCH4ToCO2e(double ch4) {
-        return (ch4 * CH4_GWP) / G_TO_KG;
+        return (ch4 * CH4_GWP) / INC_METRIC_MAGNITUDE;
     }
 
     // Method to convert N2O emissions to CO2e in kg
     public double convertN2OToCO2e(double n2o) {
-        return (n2o * N2O_GWP) / G_TO_KG;
+        return (n2o * N2O_GWP) / INC_METRIC_MAGNITUDE;
     }
 
-    // Method to only add CO2 emissions to total CO2e
+    // Method to only add CO2 emissions to total CO2e in metric tons
     // This can be used universally with total overall and total category emissions
     public double addToTotal(double total, double toAdd) {
-        return total + toAdd;
+        return (total + toAdd) / INC_METRIC_MAGNITUDE;
     }
 
-    // Method to only add CO2 emissions to total CO2e
+    // Method to only add CO2 emissions to total CO2e in metric tons
     // This can be used universally with total overall, scope, and category emissions
     public double addToTotal(double total, Map<String, Double> toAdd) {
-        return total += toAdd.get("CO2") + this.convertCH4ToCO2e(toAdd.get("CH4")) + this.convertN2OToCO2e(toAdd.get("N2O"));
+        return (total += toAdd.get("CO2") + this.convertCH4ToCO2e(toAdd.get("CH4")) + this.convertN2OToCO2e(toAdd.get("N2O"))) / INC_METRIC_MAGNITUDE;
     }
 
     // Method to report all compiled emissions based on a single gas
